94cc85354616270a0b24131a05649d20e76ed73b
a10a010 <49440663+a10a010@users.noreply.github.com>
2019-11-30 16:06:50 +0800

    Update sender1_datalink.c

--- 1751561-G00102/01/sender1_datalink.c
+++ 1751561-G00102/01/sender1_datalink.c
@@ -1,1 +1,14 @@
-
+include "../common/common.h"
+  
+int main()
+{
+  frame s;
+  packet buffer;
+  
+  while(true){
+    from_network_layer(&buffer);
+    s.info=buffer;
+    to_physical_layer(&s);
+  }
+  return 0;
+}
