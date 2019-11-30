1efda8d14ba794b3c5738f00a6739c17528cd299
Mer <40347659+MerSky@users.noreply.github.com>
2019-11-30 13:14:17 +0800

    Update common.h

--- 1751561-G00102/common/common.h
+++ 1751561-G00102/common/common.h
@@ -16,8 +16,8 @@
 #define MAX_PKT 1024 //每一帧最大容量
 #define MAX_FILE_LEN 128 //最大共享文件名长度
 #define MAX_SEQ 9999 //共享文件名数字
-#define N_D_SHARE "nerwork_datalink_share"
-#define D_N_SHARE "datalink_network_share"
+#define N_D_SHARE "nerwork_datalink_share."
+#define D_N_SHARE "datalink_network_share."
 
 typedef enum {
 	false,		//false=0
