18054624a5b548e39cf5870f80ca38de63e924d8
nood1e <49440145+nood1e@users.noreply.github.com>
2019-11-30 18:58:42 +0800

    Update common.c

--- 1751561-G00102/common/common.c
+++ 1751561-G00102/common/common.c
@@ -2,124 +2,155 @@
 
 /*------------------------------------------牟------------------------------------------*/
 /*设置文件锁*/
-int set_lock(int fd,int type)  
-{  
-    struct flock old_lock,lock;  
-    lock.l_whence = SEEK_SET;  
-    lock.l_start = 0;  
-    lock.l_len = 0;  
-    lock.l_type = type;  
-    lock.l_pid = -1;  
-    if ((fcntl(fd,F_SETLKW,&lock)) < 0)  
-    {  
-        printf("Lock failed");  
-        return 1;  
-    }  
-    return 0;  
-}  
+int set_lock(int fd, int type)
+{
+	struct flock old_lock, lock;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_type = type;
+	lock.l_pid = -1;
+	if ((fcntl(fd, F_SETLKW, &lock)) < 0)
+	{
+		printf("Lock failed");
+		return 1;
+	}
+	return 0;
+}
 
 /*使k在[1 ~ MAX_SEQ-1]间循环增长*/
 int inc_seq_num(int &seq_num)
 {
-	if(seq_num<10000)
+	if (seq_num < 10000)
 		seq_num++;
 	else
-		seq_num=0;
+		seq_num = 0;
 	return seq_num;
 }
 
 
-int key_from_network_layer=0;//信号阻塞
+int key_from_network_layer = 0;//信号阻塞
 void key_from_network_layer_enable()//信号启用
 {
-	key_from_network_layer=1;
+	key_from_network_layer = 1;
 }
 
 /*发送方从网络层得到纯数据包*/
 void from_network_layer(packet* p)
 {
-	while(key_from_physical_layer==0)();//信号阻塞
-	key_from_physical_layer=0;
+	while (key_from_physical_layer == 0)();//信号阻塞
+	key_from_physical_layer = 0;
 
-	static int seq_num=1;
+	static int seq_num = 1;
 	int fd;
 	char share_filename[MAX_FILE_LEN];
-	sprintf(share_filename,"%s%04d",N_D_SHARE,seq_num);
+	sprintf(share_filename, "%s%04d", N_D_SHARE, seq_num);
 	inc(seq_num);
-	fd=open(share_filename,O_RDONLY);
-	set_lock(fd,F_RDLCK);
-	read(share_file,p->data,MAX_PKT);
-	set_lock(fd,F_UNLCK);
+	fd = open(share_filename, O_RDONLY);
+	set_lock(fd, F_RDLCK);
+	read(share_file, p->data, MAX_PKT);
+	set_lock(fd, F_UNLCK);
 	close(fd);
-	int pid=FindPidByName("./sender_network");//获得网络层进程的pid
-	kill(pid,40);//发送40号信号
+	int pid = FindPidByName("./sender_network");//获得网络层进程的pid
+	kill(pid, 40);//发送40号信号
 	printf("datalink receive from network successfully\n");
 }
 
 /*接收方向网络层发送纯数据包*/
 void to_network_layer(packet* p)
 {
-	static int seq_num=1;
+	static int seq_num = 1;
 	char share_filename[MAX_FILE_LEN];
 	int d_nfd;
-	sprintf(share_filename,"%s%04d",D_N_SHARE,seq_num);
+	sprintf(share_filename, "%s%04d", D_N_SHARE, seq_num);
 	inc(seq_num);
-	d_nfd=open(share_file_name,O_WRONLY|O_CREAT,0644);
-	write(d_nfd,p->data,MAX_PKT);
+	d_nfd = open(share_file_name, O_WRONLY | O_CREAT, 0644);
+	write(d_nfd, p->data, MAX_PKT);
 	close(d_nfd);
 }
 
 /*接收方从物理层取得帧，调用本函数前已验证过校验和，若发生错误,则发送cksum_err事件
 因此只有帧正确的情况下会调用本函数*/
-int key_from_physical_layer=0;//信号阻塞
+int key_from_physical_layer = 0;//信号阻塞
 void key_from_physical_layer_enable()//信号启用
 {
-	key_from_physical_layer=1;
+	key_from_physical_layer = 1;
 }
 
 void from_physical_layer(frame* s);
 {
-	while(key_from_physical_layer==0)();//信号阻塞
-	key_from_physical_layer=0;
+	while (key_from_physical_layer == 0)();//信号阻塞
+	key_from_physical_layer = 0;
 
-	static int seq_num=1;
+	static int seq_num = 1;
 	int fd;
 	char share_filename[seq_num];
-	sprintf(share_filename,"%s%04d",P_D_SHARE,seq_num);
+	sprintf(share_filename, "%s%04d", P_D_SHARE, seq_num);
 	inc(seq_num);//读取文件序号+1
-	fd=open(share_filename,O_RDONLY);//打开文件
-	set_lock(fd,F_RDLCK);//设置文件锁
-	read(fd,*(s->kind),4);//帧类型
-	read(fd,*(s->seq),4);//发送序号
-	read(fd,*(s->ack),4);//接收序号
-	if (s->kind==frame_kind)read(fd,s->info->data,MAX_PKT);//数据包
-	set_lock(fd,F_UNLCK);//解开文件锁
+	fd = open(share_filename, O_RDONLY);//打开文件
+	set_lock(fd, F_RDLCK);//设置文件锁
+	read(fd, *(s->kind), 4);//帧类型
+	read(fd, *(s->seq), 4);//发送序号
+	read(fd, *(s->ack), 4);//接收序号
+	if (s->kind == frame_kind)read(fd, s->info->data, MAX_PKT);//数据包
+	set_lock(fd, F_UNLCK);//解开文件锁
 	close(fd);
 	printf("数据链路层已从物理层读入帧数据\n");
-}	
+}
 
 /*发送方向物理层发送帧*/
 void to_physical_layer(frame *s);
 {
-	static int seq_num=1;
+	static int seq_num = 1;
 	char share_filename[MAX_FILE_LEN];//写入文件名
 	int fd;
-	sprintf(share_filename,"%s%04d",D_P_SHARE,s->);//文件名
+	sprintf(share_filename, "%s%04d", D_P_SHARE, s->);//文件名
 	inc(seq_num);//文件序号+1
-	fd=open(share_file_name,O_WRONLY|O_CREAT,0644);//打开文件
-	set_lock(fd,F_RDLCK);//设置文件锁
-	write(fd,*(s->kind),4);//帧类型
-	write(fd,*(s->seq),4);//发送序号
-	write(fd,*(s->ack),4);//接收序号
-	if (s->kind==frame_kind)write(fd,s->info->data,MAX_PKT);//数据包写数据
-	set_lock(fd,F_UNLCK);//解开文件锁
+	fd = open(share_file_name, O_WRONLY | O_CREAT, 0644);//打开文件
+	set_lock(fd, F_RDLCK);//设置文件锁
+	write(fd, *(s->kind), 4);//帧类型
+	write(fd, *(s->seq), 4);//发送序号
+	write(fd, *(s->ack), 4);//接收序号
+	if (s->kind == frame_kind)write(fd, s->info->data, MAX_PKT);//数据包写数据
+	set_lock(fd, F_UNLCK);//解开文件锁
 	close(fd);//关文件
-	int pid=FindPidByName("./sender_physical");//获得网络层进程的pid
-	kill(pid,40);//发送信号让物理层读文件
+	int pid = FindPidByName("./sender_physical");//获得网络层进程的pid
+	kill(pid, 40);//发送信号让物理层读文件
 	printf("数据链路层已向物理层写入文件\n");
-}					
+}
+
+void from_datalink_layer(frame *s)
+{
+	static int seq_PKT = 0;//使用静态局部变量
+	char share_file_name[MAX_FILENANE_LEN];
+	int share_file = -1;
+	sprintf(share_file_name, "%s%04d", D_P_SHARE, seq_PKT);
+	inc_seq_PKT(seq_PKT);
+	while (share_file == -1)
+	{
+		share_file = open(share_file_name, O_RDONLY);
+	}
+	/*加锁，读取文件*/
+	set_lock(share_file, F_RDLCK);
+
+	read(share_file, s, sizeof(frame));
+
+	set_lock(share_file, F_UNLCK);//读完开锁
+
+	close(share_file);
+	/*向DATALINK_LAYER发送enable 信号分离出去 由enable_network_layer完成*/
+}
 
 /*------------------------------------------冕------------------------------------------*/
+/*物理层之间传输*/
+void send_to_phy(frame *s,int sockfd)
+{
+	if (s->kind == nak || s->kind == ack)
+		write(sockfd, s, FRAMEHEAD);
+	else
+		write(sockfd, s, sizeof(frame));
+}
+
 static event_type e;
 
 static void TIMEOUT()
@@ -178,20 +209,20 @@ void disable_network_layer()
 /*------------------------------------------舟------------------------------------------*/
 //-----------------------------计时器-------------------------------
 LinkList timeL;//使用的链表
-int nowtime,nowusec;//现在是第几帧、第几毫秒
+int nowtime, nowusec;//现在是第几帧、第几毫秒
 struct timeval tv;//用于获取当前时间
 
 /*启动第k帧的定时器*/
 void start_timer(seq_nr k)
 {
-	if (k<nowtime)return;
-	Listadd(timeL,k-nowtime);
+	if (k < nowtime)return;
+	Listadd(timeL, k - nowtime);
 }
 /*停止第k帧的定时器*/
 void stop_timer(seq_nr k)
 {
-	int pos=List_find(timeL,k),e;
-	Listdelete(timeL,pos,e);
+	int pos = List_find(timeL, k), e;
+	Listdelete(timeL, pos, e);
 }
 /*启动确认包定时器*/
 void start_ack_timer(void)
@@ -207,14 +238,14 @@ void stop_ack_timer(void)
 /*对定时器进行维护*/
 void timer_keep(void)
 {
-    gettimeofday(&tv);//获取当前时间
-	if (tv.tv_usec==nowusec)return;//触发毫秒时间中断
+	gettimeofday(&tv);//获取当前时间
+	if (tv.tv_usec == nowusec)return;//触发毫秒时间中断
 	int n;
-	n=L->next->data;//获取当前第一个定时器的时间
+	n = L->next->data;//获取当前第一个定时器的时间
 	n--;//时间减一
-	if (n==0)//计时器超时
+	if (n == 0)//计时器超时
 	{
-		Listdelete(timeL,1,e);//删除第一个计时器
+		Listdelete(timeL, 1, e);//删除第一个计时器
 		//发送timeout事件，等待填入
 	}
 }
@@ -222,9 +253,9 @@ void timer_keep(void)
 /*定时器初始化，请在开始时使用*/
 void timer_create(void)
 {
-	nowtime=0;//初始化当前帧
-	
-    gettimeofday(&tv);//获取当前时间
-	nowusec=tv.tv_usec;//初始化当前时间
+	nowtime = 0;//初始化当前帧
+
+	gettimeofday(&tv);//获取当前时间
+	nowusec = tv.tv_usec;//初始化当前时间
 }
 
