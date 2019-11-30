6535695c48ed0b020cfdbc02d0a441b69c327075
Mer <40347659+MerSky@users.noreply.github.com>
2019-12-01 02:05:12 +0800

    Add files via upload

---
+++ 1751561-G00102/03/sender3_physical.c
@@ -0,0 +1,48 @@
+#include "../common/common.h"
+//#include "common.h"
+
+static int r_en = 0;//为可读信号，链路层写完文件发送信号告知物理层可读取，此时r_en由0变1，读完再变回0
+static void EnPhy(int signo)
+{
+	r_en = 1;
+}
+
+
+
+int main()
+{
+	prctl(PR_SET_NAME, "sender_physical");
+	/*注册信号处理函数*/
+	signal(SIG_SEND_PHY_READ, EnPhy);//可读信号到来
+
+	 frame s;
+
+	 /*建立套接字*/
+	 int sockfd;
+	 int n;
+	 struct sockaddr_in sock_addr;
+	 if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		 printf("error from socket\n");
+		 return;
+	 }
+	 memset(&sock_addr, 0, sizeof(sock_addr));
+	 sock_addr.sin_family = AF_INET;
+	 sock_addr.sin_addr.s_addr = inet_addr(RIP);
+	 sock_addr.sin_port = htons(DEFAULT_PORT);
+
+	 while (connect(sockfd, (struct sockaddr*)&sock_addr, sizeof(sock_addr)) < 0) {
+		 continue;
+	 }
+
+	 /*传输文件*/
+	 while (true) {
+		 if (r_en == 0)continue;
+
+		 from_datalink_layer(&s);//从链路层读帧
+		 send_to_phy(&s, sockfd);//向另一个物理层写帧
+
+		 r_en = 0;
+	 }
+
+	 return 0;
+}
