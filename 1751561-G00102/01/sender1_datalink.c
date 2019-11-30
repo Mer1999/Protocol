ed70f0a9f606806e7169e01cbce8965cf2f98f8e
a10a010 <49440663+a10a010@users.noreply.github.com>
2019-12-01 00:36:50 +0800

    Update sender1_datalink.c

--- 1751561-G00102/01/sender1_datalink.c
+++ 1751561-G00102/01/sender1_datalink.c
@@ -4,9 +4,7 @@ int main()
 {
   frame s;
   packet buffer;
-  
   //网络层通知读文件的信号
-  extern int key_from_network_layer_enable;
   signal(SIG_SEND_LINK_READ, key_from_network_layer_enable);
   
   //死循环从网络层读文件，往物理层写文件
