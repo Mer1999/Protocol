647322e29217d9fccdf278bba8fa0cd8ec3d6cc8
nood1e <49440145+nood1e@users.noreply.github.com>
2019-11-30 18:59:01 +0800

    Update common.h

--- 1751561-G00102/common/common.h
+++ 1751561-G00102/common/common.h
@@ -18,6 +18,7 @@
 #define MAX_PKT 1024 //每一帧最大容量
 #define MAX_FILE_LEN 128 //最大共享文件名长度
 #define MAX_SEQ 9999 //共享文件名数字
+#define FRAMEHEAD 12 //nak或者ack帧的大小
 #define N_D_SHARE "network_datalink.share."
 #define D_N_SHARE "datalink_network.share."
 #define D_P_SHARE "datalink_physical.share."
@@ -43,9 +44,9 @@ typedef unsigned int seq_nr;//发送序号
 /*数据包，纯数据*/
 typedef struct {
 	unsigned char data[MAX_PKT];
-} packet; 
+} packet;
 
- /*帧类型枚举量*/
+/*帧类型枚举量*/
 typedef enum {
 	data, //数据包 
 	ack, //确认包 
@@ -58,7 +59,7 @@ typedef struct {
 	seq_nr seq;			    //发送序号 
 	seq_nr ack;			    //接收序号 
 	packet info;			//数据包
-} frame; 
+} frame;
 
 /*事件类型枚举量*/
 typedef enum {
@@ -67,26 +68,30 @@ typedef enum {
 	timeout,				//发送超时
 	network_layer_ready,	//网络层就绪 
 	ack_timeout				//确认包超时
-} event_type; 
+} event_type;
 
 /*阻塞函数，等待事件发生*/
-void wait_for_event(event_type* event);		
+void wait_for_event(event_type* event);
 /*发送方从网络层得到纯数据包*/
-void from_network_layer(packet* p);		
-/*接收方向网络层发送纯数据包,去掉帧的类型、发送/确认序号等控制信息*/	
-void to_network_layer(packet* p);	
+void from_network_layer(packet* p);
+/*接收方向网络层发送纯数据包,去掉帧的类型、发送/确认序号等控制信息*/
+void to_network_layer(packet* p);
 /*接收方从物理层取得帧，帧头尾的FLAG字节、数据中的字节填充均已去掉
 调用本函数前已验证过校验和，若发生错误,则发送cksum_err事件
-因此只有帧正确的情况下会调用本函数*/		
-void from_physical_layer(packet* p);		
+因此只有帧正确的情况下会调用本函数*/
+void from_physical_layer(packet* p);
 /*发送方向物理层发送帧,帧头尾加FLAG字节、数据中进行字节填充,计算校验和放入帧尾*/
 void to_physical_layer(frame* s);
+/*物理层读取链路层*/
+void from_datalink_layer(frame *s);
+/*物理层间传输*/
+void send_to_phy(frame *s, int sockfd);
 /*启动第k帧的定时器*/
-void start_timer(seq_nr k);	
+void start_timer(seq_nr k);
 /*停止第k帧的定时器*/
 void stop_timer(seq_nr k);
 /*启动确认包定时器*/
-void start_ack_timer(void);	
+void start_ack_timer(void);
 /*停止确认包定时器*/
 void stop_ack_timer(void);
 /*对定时器进行维护*/
@@ -98,56 +103,56 @@ void disable_network_layer(void);
 /*使k在[1 ~ MAX_SEQ-1]间循环增长,如果MAX_SEQ=1，则0/1互换*/
 #define inc(k) if(k<MAX_SEQ) k=k+1; else k=1;
 /*文件锁设置*/
-int set_lock(int fd,int type) 
+int set_lock(int fd, int type)
 
 
 
 //----------------------------------工具函数-------------------------
-void sysLocalTime() 
-{ 
-    time_t             timesec; 
-    struct tm         *p; 
-    time(×ec); 
-    p = localtime(×ec); 
-    printf("%d%d%d%d%d%d\n", 1900+p->tm_year, 1+p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec); 
-       
-} 
-   
-void sysUsecTime() 
-{ 
-    struct timeval    tv; 
-    struct timezone tz; 
-    struct tm         *p; 
-    gettimeofday(&tv, &tz); 
-    printf("tv_sec:%ld\n",tv.tv_sec); 
-    printf("tv_usec:%ld\n",tv.tv_usec); 
-    printf("tz_minuteswest:%d\n",tz.tz_minuteswest); 
-    printf("tz_dsttime:%d\n",tz.tz_dsttime); 
-       
-    p = localtime(&tv.tv_sec); 
-    printf("time_now:%d%d%d%d%d%d.%ld\n", 1900+p->tm_year, 1+p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec, tv.tv_usec); 
-} 
+void sysLocalTime()
+{
+	time_t             timesec;
+	struct tm         *p;
+	time(×ec);
+	p = localtime(×ec);
+	printf("%d%d%d%d%d%d\n", 1900 + p->tm_year, 1 + p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec);
+
+}
+
+void sysUsecTime()
+{
+	struct timeval    tv;
+	struct timezone tz;
+	struct tm         *p;
+	gettimeofday(&tv, &tz);
+	printf("tv_sec:%ld\n", tv.tv_sec);
+	printf("tv_usec:%ld\n", tv.tv_usec);
+	printf("tz_minuteswest:%d\n", tz.tz_minuteswest);
+	printf("tz_dsttime:%d\n", tz.tz_dsttime);
+
+	p = localtime(&tv.tv_sec);
+	printf("time_now:%d%d%d%d%d%d.%ld\n", 1900 + p->tm_year, 1 + p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec, tv.tv_usec);
+}
 
 //读取pid 
-int FindPidByName(const char *pName)  
-{  
-    int szPid=-1;  
-  
-    char szProQuery[256];  
-    sprintf(szProQuery,"ps -ef|grep '%s'|grep -v 'grep'|awk '{print $2}'",pName);  // 打开管道,执行shell命令  
-  
-    FILE *fp=popen(szProQuery,"r");  
-    char szBuff[10];  
-  
-    while(fgets(szBuff,10,fp)!=NULL) // 逐行读取执行结果
-    {  
-        szPid=atoi(szBuff);  
-        break;  
-    }  
-  
-    pclose(fp); // 关闭管道指针,不是fclose()很容易混淆  
-    return szPid;  
-}  
+int FindPidByName(const char *pName)
+{
+	int szPid = -1;
+
+	char szProQuery[256];
+	sprintf(szProQuery, "ps -ef|grep '%s'|grep -v 'grep'|awk '{print $2}'", pName);  // 打开管道,执行shell命令  
+
+	FILE *fp = popen(szProQuery, "r");
+	char szBuff[10];
+
+	while (fgets(szBuff, 10, fp) != NULL) // 逐行读取执行结果
+	{
+		szPid = atoi(szBuff);
+		break;
+	}
+
+	pclose(fp); // 关闭管道指针,不是fclose()很容易混淆  
+	return szPid;
+}
 
 //-----------------------------链表实现---------------------------
 #define OK           1
@@ -171,7 +176,7 @@ int ListLength(LinkList L) {
 	LinkList p = L->next;
 	int len = 0;
 	if (p == NULL)return 0;
-	while (p!=L) {
+	while (p != L) {
 		p = p->next;
 		len++;
 	}
@@ -203,7 +208,7 @@ Status Listadd(LinkList &L, ElemType e) {
 		p = p->next;
 		pos++;
 	}
-	if (p == NULL )return ERROR;
+	if (p == NULL)return ERROR;
 	s = (LinkList)malloc(sizeof(LNode));
 	if (s == NULL)return OVERFLOW;
 	s->data = e;
