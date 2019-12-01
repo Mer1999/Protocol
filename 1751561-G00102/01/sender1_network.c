7106d7a5e066cf9b062555b7f18c997c1fef262a
Mer <40347659+MerSky@users.noreply.github.com>
2019-12-01 19:51:38 +0800

    添加写日志

--- 1751561-G00102/01/sender1_network.c
+++ 1751561-G00102/01/sender1_network.c
@@ -14,13 +14,13 @@ int main(int argc,char *argv[])
 {
     sprintf(argv[0],"sender_network");
    prctl(PR_SET_NAME, "sender_network");
-   signal(SIG_ENABLE_NET, enable_network);//38
-   signal(SIG_DISABLE_NET, disable_network);//39
-   char buffer[MAX_PKT + 1] = { 0 };//存储文件中读取的数据
-   FILE* fp = fopen("test.txt", "r");//打开文件进行读
-   if (fp == NULL)//文件打开失败
+   signal(SIG_ENABLE_NET, enable_network);    //38号信号
+   signal(SIG_DISABLE_NET, disable_network);  //39号信号
+   char buffer[MAX_PKT + 1] = { 0 };          //存储文件中读取的数据
+   FILE* fp = fopen("test.txt", "r");         //打开文件进行读
+   if (fp == NULL)                            //文件打开失败，写日志并退出
    {
-      perror("open file");
+      writelog("SDL open file fail,exit -1\n");
       exit(-1);
    }
    int realread = 0;             //fread返回值，实际读取到的字符数
@@ -29,8 +29,8 @@ int main(int argc,char *argv[])
    int fd;                       //共享文件句柄
    char filename[MAX_FILE_LEN];  //共享文件名
    seq_nr filenum = 1;           //共享文件序号
-   int lock=0;
-   int pid;
+   int lock=0;                   //判断文件是否上锁
+   int pid;                      //进程号
    while (keepread)
    {
       
@@ -47,25 +47,26 @@ int main(int argc,char *argv[])
             for (i = realread; i < MAX_PKT; i++)
                buffer[i] = '\0';
          }
-         if (!realread)//最后一次循环，将keepread置零
-            keepread = 0;
-
          sprintf(filename, "%s%04d", N_D_SHARE, filenum);//组合共享文件名
          //printf("filename = %s\n", filename);
          inc(filenum);//filenum++
          fd = open(filename, O_WRONLY | O_CREAT, 0777);
-         if(fd==-1)
+         if(fd==-1)//文件打开失败，写日志并退出
          {
-          perror("open file");
+         writelog("SDL open file fail,exit -1\n");
           exit(-1);
          }
          flock(fd,LOCK_EX);
          write(fd, buffer, MAX_PKT);//将buffer内容写入共享文件
          flock(fd,LOCK_UN);
          while((pid=FindPidByName("sender_datalink"))==-1);
-         printf("%d",pid);
-         kill(pid,41);//通知链路层读共享文件
-         
+         //printf("%d",pid);
+         kill(pid,SIG_SEND_LINK_READ);//通知链路层读共享文件
+         if (!realread)//最后一次循环，将keepread置零
+         {
+            keepread = 0;
+            writelog("SDL work finish\n");
+         }
       }
       pause();
       if(ena)
@@ -73,6 +74,7 @@ int main(int argc,char *argv[])
         if(lock)
         {
           flock(fd,LOCK_UN);
+          writelog("SDL unlock file\n");
           lock=0;
         }
         close(fd);
@@ -82,11 +84,11 @@ int main(int argc,char *argv[])
         if(!lock)
         {
           flock(fd,LOCK_EX);
+          writelog("SDL lock file\n");
           lock=1;
         }
       }
    }
-
    fclose(fp);
    return(0);
 }
