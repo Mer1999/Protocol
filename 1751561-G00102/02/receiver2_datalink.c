fb6bd1a2939001fffd81dcbb8a6c58cb07d3c7c8
a10a010 <49440663+a10a010@users.noreply.github.com>
2019-12-01 20:16:02 +0800

    Update receiver2_datalink.c

--- 1751561-G00102/02/receiver2_datalink.c
+++ 1751561-G00102/02/receiver2_datalink.c
@@ -1,17 +1,19 @@
 #include "../common/common.h"
   
-int main()
+int main(int argc,char *argv[])
 {
-  frame r;
+    sprintf(argv[0],"receiver_datalink");
+    prctl(PR_SET_NAME, "receiver_datalink");
+  frame r,s;
   event_type event;
   //物理层通知读文件的信号
   signal(SIG_RECV_LINK_READ, key_from_physical_layer_enable);
-  
   //死循环从物理层读文件，往网络层写文件
   while(true){
     wait_for_event(&event);
     from_physical_layer(&r);
     to_network_layer(&r.info);
+    to_physical_layer(&s);
   }
   return 0;
 }
